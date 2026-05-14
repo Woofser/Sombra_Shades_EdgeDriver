a21676248c4aebdd4994dfdd73f4834887d14f2d
Woofser <rizner@outlook.com>
2026-05-14 22:53:03 +0000

    Updated package

--- package.sh
+++ package.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 PACKAGE_NAME="sombra-zigbee-edge-driver.zip"
 rm -f "$PACKAGE_NAME"
-zip -r "$PACKAGE_NAME" main.lua metadata.lua README.md package.sh sub_drivers drivers
+zip -r "$PACKAGE_NAME" config.yaml main.lua metadata.lua README.md package.sh sub_drivers drivers
 
 echo "Created package: $PACKAGE_NAME"
